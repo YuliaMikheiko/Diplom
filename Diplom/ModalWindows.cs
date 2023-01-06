f3921130c9702a756c4ee708ad4b3679937ced3a
YuliaMikheiko <yu_lu26@mail.ru>
2023-01-06 22:46:00 +0800

    Модальные окна групп и аудиторий

--- Diplom/ModalWindows.cs
+++ Diplom/ModalWindows.cs
@@ -64,28 +64,54 @@ namespace WindowsFormsApp2
 
         private void ReadGroups()
         {
-           
             TeachersDGV.Visible = false;
             GroupsDGV.Visible = true;
             AuditorysDVG.Visible = false;
 
+            GroupsDGV.RowCount = data.sub_groups_info.Count;
             GroupsDGV.ColumnCount = 3;
+            var i = 0;
+            foreach (var groups in data.sub_groups_info)
+            {
+                GroupsDGV.Rows[i].Cells[1].Value = groups.Value.id;
+                GroupsDGV.Rows[i].Cells[2].Value = groups.Value.title;
+                for (int j = 0; j < data.nagr[row].sub_groups.Length; j++)
+                {
+                    if (data.nagr[row].sub_groups[j] == groups.Value.id)
+                    {
+                        GroupsDGV.Rows[i].Cells[0].Value = true;
+                    }
+                }
+                i++;
+            }
         }
 
         private void ReadAuditorys()
         {
-
             TeachersDGV.Visible = false;
             GroupsDGV.Visible = false;
             AuditorysDVG.Visible = true;
 
+            AuditorysDVG.RowCount = data.auditories.Count;
             AuditorysDVG.ColumnCount = 3;
+            var i = 0;
+            foreach (var auditorys in data.auditories)
+            {
+                AuditorysDVG.Rows[i].Cells[1].Value = auditorys.Value.id;
+                AuditorysDVG.Rows[i].Cells[2].Value = auditorys.Value.title;
+                for (int j = 0; j < data.nagr[row].auds.Length; j++)
+                {
+                    if (data.nagr[row].auds[j] == auditorys.Value.id)
+                    {
+                        AuditorysDVG.Rows[i].Cells[0].Value = true;
+                    }
+                }
+                i++;
+            }
         }
 
         private void Choice_Click(object sender, EventArgs e)
         {
-            var g = data.nagr[row];
-
             if (type == 1)
             {
                 List<int?> list = new List<int?>();
@@ -111,8 +137,59 @@ namespace WindowsFormsApp2
                     f1.dataGridView1.Rows[row].Cells[2].Value = value;
                 }
             }
-            this.Hide();
 
+            if (type == 2)
+            {
+                List<int> list = new List<int>();
+                foreach (DataGridViewRow rows in GroupsDGV.Rows)
+                {
+                    if (rows.Cells[0].Value != null)
+                    {
+                        if ((bool)rows.Cells[0].Value)
+                        {
+                            list.Add((int)rows.Cells[1].Value);
+                        }
+                    }
+
+                }
+                if (list.Count > 0)
+                {
+                    data.nagr[row].sub_groups = list.ToArray();
+                    var value = "";
+                    foreach (var l in list)
+                    {
+                        value += l.ToString() + " ";
+                    }
+                    f1.dataGridView1.Rows[row].Cells[4].Value = value;
+                }
+            }
+
+            if (type == 3)
+            {
+                List<int?> list = new List<int?>();
+                foreach (DataGridViewRow rows in AuditorysDVG.Rows)
+                {
+                    if (rows.Cells[0].Value != null)
+                    {
+                        if ((bool)rows.Cells[0].Value)
+                        {
+                            list.Add((int)rows.Cells[1].Value);
+                        }
+                    }
+                }
+                if (list.Count > 0)
+                {
+                    data.nagr[row].auds = list.ToArray();
+                    var value = "";
+                    foreach (var l in list)
+                    {
+                        value += l.ToString() + " ";
+                    }
+                    f1.dataGridView1.Rows[row].Cells[6].Value = value;
+                }
+            }
+
+            this.Hide();
         }
     }
 }
